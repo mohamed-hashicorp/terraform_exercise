13f4b4d497e331392ab7c10a9f6974158a6e84e5
mohamed-hashicorp <mohamed.abdelbaset@hashicorp.com>
2025-10-24 10:54:56 +0200

    adding a parent readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Terraform Up & Running — Sample Code Practice
+
+This repository contains example Terraform configurations and exercises inspired by the book **“Terraform: Up & Running”** by Yevgeniy Brikman.  
+
+## Quick start
+
+All the code is in these folders. The code examples are organized 
+by chapters. For example, if you're looking at an example of Terraform code in Chapter 2 deploying a single server, you'll find it in the 
+[chapter2/one-server](chapter2/one-server) folder.
+
+For instructions on running the code, please consult the README in each folder, and, of course, the
+*[Terraform: Up and Running](http://www.terraformupandrunning.com)* book.
+
+
--- chapter3/file-layout-example/global/s3/main.tf
+++ chapter3/file-layout-example/global/s3/main.tf
@@ -1,28 +1,26 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-ayman"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-2"
-
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
-
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state-ayman"
 
-  # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = false
-  }
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
 }
 
 # Enable versioning so you can see the full revision history of your
@@ -55,21 +53,12 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
   }
-}
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
 }
